ce0196634817e34389c3e7ddf02c8e8329e95abc
Facundo Vera <facundovera.dev@gmail.com>
2026-03-05 03:37:59 -0300

    Creo la vista de canchas con su componente HeroField.jsx por separado y estilos css

---
+++ src/components/HeroField.jsx
@@ -0,0 +1,26 @@
+import cespedcancha from "../assets/cespedcancha.webp";
+import "../css/herofield.css";
+
+const HeroField = () => {
+  return (
+    <section
+      className="hero-field  d-flex justify-content-center text-white position-relative"
+      style={{ backgroundImage: `url(${cespedcancha})`, 
+    zIndex:`-10`}}
+    >
+      <h1 className="hero-title text-center mt-3">Canchas Futbol - 5</h1>
+
+      <div className="position-absolute bottom-0 start-0 px-3 py-5 d-flex flex-column gap-2">
+        <span className="hero-badge rounded-pill px-3 align-self-start">
+          Abierto 19:00 a 00:00
+        </span>
+
+        <span className="hero-badge rounded-pill px-3 align-self-start">
+          Estacionamiento - Parrilla - Vestuarios
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default HeroField;
---
+++ src/views/public/Fields.jsx
@@ -0,0 +1,17 @@
+import HeroField from "../../components/HeroField"
+import "../../css/fields.css"
+
+
+
+const Fields = () => {
+  return (
+    <div>
+        <HeroField/>
+        <section className="section-fields">
+          <h2 className="text-center py-3 title">Nuestras canchas</h2>
+        </section>
+    </div>
+  )
+}
+
+export default Fields
