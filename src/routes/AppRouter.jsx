94bb8d6efc748e8aaf3e5de94944b638aae725a9
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2026-04-13 22:14:10 -0300

    fix: protect ecommerce routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -24,13 +24,13 @@ export const AppRouter = () => {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/about" element={<AboutScreen />} />
         <Route path="/contact" element={<ContactScreen />} />
-        <Route path="/ecommerce" element={<EcommerceView />} />
-        <Route path="/producto/:id" element={<ProductDetailView />} />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<ModalRegistro />} />
 
         <Route element={<ProtectedRoute adminOnly={false} />}>
           <Route path="/fields" element={<Fields />} />
+          <Route path="/ecommerce" element={<EcommerceView />} />
+          <Route path="/producto/:id" element={<ProductDetailView />} />
           <Route path="/cart" element={<CartView />} />
           <Route path="/my-bookings" element={<MyBookingsView />} />
         </Route>
